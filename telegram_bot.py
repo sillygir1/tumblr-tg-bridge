eb406eda0b4a97a122d793ac67c67e07f5c4b1f5
sillygir1 <25554397+sillygir1@users.noreply.github.com>
2025-08-01 02:52:13 +0400

    Add answers. Many unlisted improvements.

--- telegram_bot.py
+++ telegram_bot.py
@@ -10,6 +10,19 @@ import tumblr_post
 base_url = f'https://api.telegram.org/bot{config.telegram_api_key}'
 
 
+def send_text_post(post_text: str):
+    response = requests.post(
+        f'{base_url}/sendMessage',
+        params={
+            'chat_id': config.telegram_chat_id,
+            'parse_mode': 'MarkdownV2',
+            'link_preview_options': '{"is_disabled": true}',
+            'text': post_text,
+        }
+    )
+    # print(response.content)
+
+
 if not os.path.isfile(config.timestamp_file_path):
     with open(config.timestamp_file_path, 'x') as time_file:
         time_file.write(str(int(time())))
@@ -21,7 +34,7 @@ with open(config.timestamp_file_path, 'r') as time_file:
 tumblr_client = pytumblr.TumblrRestClient(*config.tumblr_secret)
 
 latest_posts = tumblr_client.posts(config.blog_name,
-                                   type='text',
+                                   #    type='text',
                                    limit=5)['posts'][::-1]
 
 for post in latest_posts:
@@ -29,26 +42,34 @@ for post in latest_posts:
 
     if post.timestamp < last_time:
         continue
-    # Commented for testing purposes
-    # if post.state == 'private':
-        # continue
+    if post.state == 'private':
+        continue
+
+    # print(f'{post.type=}')
 
     if post.type == 'text':
+        # print(post)
         parsed_post = tumblr_post.TextPost(post)
+        match (parsed_post.media_count):
+            case 0:
+                post_text = parsed_post.prettify()
+                # print(post_text)
+                send_text_post(post_text)
+                # print('')
+                # print('')
+            case 1:
+                # TODO image post
+                pass
+            case _:
+                # Multiple images. Impossible with current technology.
+                continue
+    elif post.type == 'answer':
+        parsed_post = tumblr_post.AnswerPost(post)
         post_text = parsed_post.prettify()
         # print(post_text)
+        send_text_post(post_text)
         # print('')
         # print('')
-        response = requests.post(
-            f'{base_url}/sendMessage',
-            params={
-                'chat_id': config.telegram_chat_id,
-                'parse_mode': 'Markdown',
-                'link_preview_options': '{"is_disabled": true}',
-                'text': post_text,
-            }
-        )
-        print(response.content)
 
 
 with open(config.timestamp_file_path, 'w+') as time_file:
--- tumblr_post.py
+++ tumblr_post.py
@@ -5,21 +5,26 @@ from urllib.parse import urlparse
 import dotmap
 import re
 import telegram
+import telegramify_markdown
 
 media_url_regex = r"(https?://[^\s)]+\.[a-z]{3,4})"
+
+
+def format_blog_url(blog: str):
+    blog = blog.replace('-', '\-')
+    return f'[{blog}]({blog}.tumblr.com)'
 
 
 class TumblrPostTrail:
     def __init__(self, post_trail):
         self.blog = post_trail.blog.name
         self.content = html2text.html2text(post_trail.content, bodywidth=0)
-        self.media_present = self._detect_media_()
+        self._detect_media_()
 
     def _detect_media_(self):
-        media = re.findall(media_url_regex, self.content)
-        if media == []:
+        self.media = re.findall(media_url_regex, self.content)
+        if self.media == []:
             return False
-        self.media = media
         return True
 
 
@@ -35,43 +40,91 @@ class TumblrPost:
             blog_name = post_url.netloc.split('.')[0]
             if blog_name == 'www':
                 blog_name = post_url.path.split('/')[-2]
-                if blog_name == self.author:
-                    blog_name = ''
+            if blog_name == self.author:
+                blog_name = ''
             self.reblog_source = blog_name
         else:
             self.is_reblog = False
         self.trail = [TumblrPostTrail(trail) for trail in post_data.trail]
 
-        self.media_present = any([trail.media_present for trail in self.trail])
+        self.media_count = sum([len(trail.media) for trail in self.trail])
         self.tags = post_data.tags
         self.blog = post_data.blog_name
         self.timestamp = post_data.timestamp
+
+    def _format_header_(self) -> str:
+        header_buffer = ''
+        if self.is_reblog:
+            header_buffer += f'{format_blog_url(config.blog_name)} 🔁'
+            if self.reblog_source:
+                header_buffer += f' {format_blog_url(self.reblog_source)}'
+            header_buffer += '\n'
+        return header_buffer
+
+    def _format_trail_(self, start: int = 0):
+        trail_buffer = ''
+        for trail in self.trail[start:]:
+            content = telegramify_markdown.markdownify(
+                trail.content.replace('\n\n', '\n'))
+            trail_buffer += f'{format_blog_url(trail.blog)}:\n{content}\n'
+        return trail_buffer
+
+    def _format_tags_(self):
+        tag_buffer = ''
+        for tag in self.tags:
+            tag = telegramify_markdown.markdownify(tag)
+            tag_buffer += (f'[\#{tag}](tumblr.com/tagged/{tag}) ')
+        return tag_buffer
 
 
 class TextPost(TumblrPost):
     def prettify(self):
         post_buffer = ''
-        if self.is_reblog:
-            post_buffer += f'{config.blog_alias} 🔁'
-            if self.reblog_source:
-                post_buffer += f' [{self.reblog_source}]({self.reblog_source}.tumblr.com)'
-            post_buffer += '\n'
-        for trail in self.trail:
-            content = telegram.helpers.escape_markdown(
-                trail.content.strip(), 1)
-            post_buffer += f'[{trail.blog}]({trail.blog}.tumblr.com):\n{content}\n\n'
 
-        for tag in self.tags:
-            post_buffer += f'[#{tag}](tumblr.com/tagged/{tag}) '
+        post_buffer += self._format_header_()
+        post_buffer += self._format_trail_()
+        post_buffer += self._format_tags_()
 
         return post_buffer
 
 
-class ImagePost(TumblrPost):
-    ...
+class AnswerPost(TumblrPost):
+    def __init__(self, post_data):
+        super().__init__(post_data)
+        self.content = ''
+        self.asking_name = post_data.asking_name
+        self.question = html2text.html2text(
+            post_data.question).replace('\n\n', '\n')
+        self.answer = html2text.html2text(
+            post_data.answer).replace('\n\n', '\n')
+
+    def prettify(self):
+        post_buffer = ''
+        post_buffer += self._format_header_()
+        if self.asking_name == '>Anonymous':
+            post_buffer += f'{self.asking_name}'
+        else:
+            post_buffer += f'[{self.asking_name}]({self.asking_name}.tumblr.com)'
+        post_buffer += ' asked:\n'
+        if self.question.endswith('\n'):
+            self.question = self.question.removesuffix('\n')
+        post_buffer += f'>{telegramify_markdown.markdownify(self.question.replace('\n', '\n>'))}\n'
+
+        post_buffer += f'[{self.trail[0].blog}]({self.trail[0].blog}.tumblr.com)'
+        post_buffer += ' answered:\n'
+        answer = html2text.html2text(
+            self.trail[0].content).replace('\n\n', '\n')
+        post_buffer += f'{telegramify_markdown.markdownify(answer)}\n'
+
+        if len(self.trail) == 1:
+            return post_buffer
+
+        post_buffer += self._format_trail_(1)
+
+        return post_buffer
 
 
-class OtherPost(TumblrPost):
+class ImagePost(TextPost):
     ...
 
 
